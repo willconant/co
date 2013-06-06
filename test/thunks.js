3bada1b0cb388dfaffb733557178d22b20728a4a
Will Conant <will.conant@gmail.com>
2013-06-06 16:05:34 -0600

    split promise tests into their own file

--- test/thunks.js
+++ test/thunks.js
@@ -12,13 +12,6 @@ function get(val, err, error) {
   }
 }
 
-function getPromise(val, err) {
-  return Q.fcall(function(){
-    if (err) throw err;
-    return val;
-  });
-}
-
 describe('co(fn)', function(){
   describe('with no yields', function(){
     it('should work', function(done){
@@ -38,36 +31,12 @@ describe('co(fn)', function(){
     })
   })
 
-  describe('with one promise yield', function(){
-    it('should work', function(done){
-      co(function *(){
-        var a = yield getPromise(1);
-        a.should.equal(1);
-        done();
-      });
-    })
-  })
-
   describe('with several yields', function(){
     it('should work', function(done){
       co(function *(){
         var a = yield get(1);
         var b = yield get(2);
         var c = yield get(3);
-
-        [a,b,c].should.eql([1,2,3]);
-
-        done();
-      });
-    })
-  })
-
-  describe('with several promise yields', function(){
-    it('should work', function(done){
-      co(function *(){
-        var a = yield getPromise(1);
-        var b = yield getPromise(2);
-        var c = yield getPromise(3);
 
         [a,b,c].should.eql([1,2,3]);
 
@@ -118,25 +87,6 @@ describe('co(fn)', function(){
 
         assert('boom' == error.message);
         var ret = yield get(1);
-        assert(1 == ret);
-        done();
-      });
-    })
-  })
-
-  describe('when a promise is rejected', function(){
-    it('should throw and resume', function(done){
-      var error;
-
-      co(function *(){
-        try {
-          yield getPromise(1, new Error('boom'));
-        } catch (err) {
-          error = err;
-        }
-
-        assert('boom' == error.message);
-        var ret = yield getPromise(1);
         assert(1 == ret);
         done();
       });
